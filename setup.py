f7e87fe1f4bc67c36e37c5b8fb632de081c8a40f
Martin Fischer <MFischer@anexia-it.com>
2022-10-21 13:43:34 +0200

    add test app, test and publish workflows

--- setup.py
+++ setup.py
@@ -23,20 +23,22 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     install_requires=[
-        'awsiotsdk'
+        'awsiotsdk',
+        'django',
     ]
 )
